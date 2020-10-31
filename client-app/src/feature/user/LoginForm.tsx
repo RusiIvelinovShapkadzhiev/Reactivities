5a375b516a539a640a64c4ba66a6cd9e9c484b32
unknown <dimitria_d@abv.bg>
2020-10-31 03:47:49 +0200

    Facebook login feature added

--- client-app/src/feature/user/LoginForm.tsx
+++ client-app/src/feature/user/LoginForm.tsx
@@ -1,12 +1,14 @@
 import { FORM_ERROR } from 'final-form';
+import { observer } from 'mobx-react-lite';
 import React, { useContext } from 'react';
 import { Form as FinalForm, Field} from 'react-final-form';
 import { combineValidators, isRequired } from 'revalidate';
-import { Button, Form, Header } from 'semantic-ui-react';
+import { Button, Divider, Form, Header } from 'semantic-ui-react';
 import { ErrorMessage } from '../../app/common/form/ErrorMessage';
 import TextInput from '../../app/common/form/TextInput';
 import { IUserFormValues } from '../../app/models/user';
 import { RootStoreContext } from '../../app/stores/rootStore';
+import SocialLogin from './SocialLogin';
 
 const validate = combineValidators({
     email: isRequired('email'),
@@ -15,7 +17,7 @@ const validate = combineValidators({
 
 const LoginForm = () => {
     const rootStore = useContext(RootStoreContext);
-    const { login } = rootStore.userStore;
+    const { login, fbLogin, loading } = rootStore.userStore;
 
     return (
         <FinalForm 
@@ -65,10 +67,12 @@ const LoginForm = () => {
                     />
                     {/* <pre>{JSON.stringify(form.getState(), null, 2)}</pre>
                     <pre>{JSON.stringify(submitting.valueOf(), null, 2)}</pre> */}
+                    <Divider horizontal>Or</Divider>
+                    <SocialLogin loading={loading} fbCallback={fbLogin}/>
                 </Form>
             )}
         />
     )
 }
 
-export default LoginForm;
+export default observer(LoginForm);
